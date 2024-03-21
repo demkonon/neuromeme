ee8dc1adad78d64b549ea1b8bb2efb737b814a28
Даниил <daniil@MacBook-Pro-Daniil.local>
2024-03-21 16:19:01 +0300

    Commit 21.03.2024

--- run.py
+++ run.py
@@ -7,42 +7,54 @@ from server.babel import create_babel
 from json import load
 from flask import Flask
 
-if __name__ == '__main__':
 
-    # Load configuration from config.json
-    config = load(open('config.json', 'r'))
-    site_config = config['site_config']
-    url_prefix = config.pop('url_prefix')
+def start_app():
 
-    # Create the app
+    # Определение Flask-приложения
     app = Flask(__name__)
     app.secret_key = secrets.token_hex(16)
 
-    # Set up Babel
+    # Настройка Babel
     create_babel(app)
 
-    # Set up the website routes
-    site = Website(bp, url_prefix)
-    for route in site.routes:
-        bp.add_url_rule(
-            route,
-            view_func=site.routes[route]['function'],
-            methods=site.routes[route]['methods'],
-        )
+    # Настройка маршрутов сайта
+    site_config = None
+    url_prefix = None
+    site = None
+    backend_api = None
 
-    # Set up the backend API routes
-    backend_api = Backend_Api(bp, config)
-    for route in backend_api.routes:
-        bp.add_url_rule(
-            route,
-            view_func=backend_api.routes[route]['function'],
-            methods=backend_api.routes[route]['methods'],
-        )
+    # Функция для загрузки конфигурации и настройки приложения
+    def setup_app():
+        global site_config, url_prefix, site, backend_api
+        # Загрузка конфигурации из config.json
+        config = load(open('config.json', 'r'))
+        site_config = config['site_config']
+        url_prefix = config.pop('url_prefix')
 
-    # Register the blueprint
-    app.register_blueprint(bp, url_prefix=url_prefix)
+        # Настройка маршрутов сайта
+        site = Website(bp, url_prefix)
+        for route in site.routes:
+            bp.add_url_rule(
+                route,
+                view_func=site.routes[route]['function'],
+                methods=site.routes[route]['methods'],
+            )
+        # Настройка маршрутов API
+        backend_api = Backend_Api(bp, config)
+        for route in backend_api.routes:
+            bp.add_url_rule(
+                route,
+                view_func=backend_api.routes[route]['function'],
+                methods=backend_api.routes[route]['methods'],
+            )
+        # Регистрация blueprint
+        app.register_blueprint(bp, url_prefix=url_prefix)
 
-    # Run the Flask server
-    print(f"Running on {site_config['port']}{url_prefix}")
-    app.run(**site_config)
-    print(f"Closing port {site_config['port']}")
+        print(f"Running on {site_config['port']}{url_prefix}")
+        app.run(**site_config)
+        print(f"Closing port {site_config['port']}")
+
+
+    setup_app()
+
+start_app()
